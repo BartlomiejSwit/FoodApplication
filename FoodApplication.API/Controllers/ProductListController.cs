1eab64fdfb9c20f897bd27341ec6a967f9859eeb
BartłomiejSwit <switbartlomiej@gmail.com>
2023-01-18 20:53:34 +0100

    again

---
+++ FoodApplication.API/Controllers/ProductListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodApplication.API.Controllers
+{
+    public class ProductListController : Controller
+    {
+        // GET: ProductList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
