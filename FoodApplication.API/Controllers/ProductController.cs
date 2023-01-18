b860c6430d88f9dd8dfc8cf69ab1f8a9ed29657f
BartłomiejSwit <switbartlomiej@gmail.com>
2023-01-18 21:21:42 +0100

    Add & edit controlers

--- FoodApplication.API/Controllers/ProductController.cs
+++ FoodApplication.API/Controllers/ProductController.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace FoodApplication.API.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : ApiController
     {
-        // GET: Product
-        public ActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
--- FoodApplication.API/Controllers/ProductListController.cs
+++ FoodApplication.API/Controllers/ProductListController.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace FoodApplication.API.Controllers
 {
-    public class ProductListController : Controller
+    public class ProductListController : ApiController
     {
-        // GET: ProductList
-        public ActionResult Index()
+
+        // GET: api/ProductList/5
+        public string Get(string id)
         {
-            return View();
+            return id;
+        }
+
+        // POST api/ProductList
+        public string Post([FromBody] string value)
+        {
+            return value;
         }
     }
 }
