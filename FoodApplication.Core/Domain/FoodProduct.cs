5bde094abd04330ba3decf880b50e9845fec5a84
BartłomiejSwit <switbartlomiej@gmail.com>
2023-02-01 21:20:09 +0100

    folders end klas and enum

---
+++ FoodApplication.Core/Domain/FoodProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApplication.Core.Domain
+{
+    public class FoodProduct
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int quantity { get; set; }  
+        public MesaureUnit mesaureUnit { get; set; }
+
+
+  
+            
+
+
+    }
+}
---
+++ FoodApplication.Core/Domain/MesaureUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodApplication.Core.Domain
+{
+    public enum MesaureUnit
+    {
+        szt, 
+        kg, 
+        g, 
+        dk, 
+        l, 
+        ml
+    }
+}
